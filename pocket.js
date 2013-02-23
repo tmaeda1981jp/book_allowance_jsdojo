a5717d192f39d14eff3d267f94e17d2b4870ed2c
tmaeda1981jp <tmaeda1981jp@gmail.com>
2013-02-23 17:25:32 +0900

    'month'で月ごとの収支を表示できるようにした．

--- pocket.js
+++ pocket.js
@@ -4,14 +4,13 @@
 
 var host = 'http://localhost:3000/',
     request = require('request'),
-    params = process.argv.slice(2)
+    params = process.argv.slice(2),
+    item
 ;
 
 switch(params[0]) {
 case 'add':
-  var item = {
-    amount: params[1]
-  };
+  item = { amount: params[1] };
   if (typeof params[2] !== 'undefined') {
     item.date = new Date(params[2]+'-00:00:00').getTime();
   }
@@ -21,6 +20,7 @@ case 'add':
     }
   });
   break;
+
 case 'list':
   request.get(host + 'list', function(error, response, body) {
     var result = JSON.parse(response.body);
@@ -30,6 +30,26 @@ case 'list':
     });
   });
   break;
+
+case 'month':
+  item = {};
+  if (typeof params[1] !== 'undefined') {
+    var year = new Date().getFullYear(),
+        yyyymm = year + ('0' + parseInt(params[1], 10)).slice(-2);
+    item.month = yyyymm;
+    request.get(host + 'month', item, function(error, response, body) {
+      var result = JSON.parse(response.body), sum;
+      result.forEach(function(item) {
+        sum += item.amount;
+      });
+      console.log(yyyymm + 'の収支: ' + sum + '円');
+    });
+  }
+  else {
+    console.log("ex) node pocket month 12");
+  }
+  break;
+
 default:
-  console.log("");
+  console.log("Usage)\n node pocket [add|list|month] params\n");
 }
