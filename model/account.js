07f3a09a159255e9464ea1cbf4d4f6862693a926
naoki kodama <naoki.koda@gmail.com>
2013-02-23 17:33:00 +0900

    月表示のサーバサイドを実装。

--- model/account.js
+++ model/account.js
@@ -8,12 +8,12 @@ var ItemSchema = new mongoose.Schema({
   amount: Number
 });
 mongoose.model('Item', ItemSchema);
+var Item = mongoose.model('Item');
 
 function Account(db_name) {
   mongoose.connect('mongodb://localhost/' + db_name);
 
   this.add = function (item) {
-    var Item = mongoose.model('Item');
     var new_item = new Item();
     new_item.date = item.date;
     new_item.amount = item.amount;
@@ -23,8 +23,17 @@ function Account(db_name) {
   }
 
   this.list = function(callback){
-    var Item = mongoose.model('Item');
     Item.find({}, function(err, items){
+      callback(items);
+    });
+  }
+
+  this.month = function(month, callback){
+    var req_date = month;
+    var show_date = req_date.match(/(\d{4})(\d{2})/);
+    var start_date = new Date(show_date[1],parseInt(show_date[2]) - 1, 1,0,0,0);
+    var next_date = new Date(show_date[1],parseInt(show_date[2]) ,1,0,0,0);
+    Item.find({date:{$gte: start_date, $lte: next_date}},function(err, items){
       callback(items);
     });
   }
