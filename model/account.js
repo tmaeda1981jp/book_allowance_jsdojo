3694b00691c772c6234961133b244e0d5c62128d
naoki kodama <naoki.koda@gmail.com>
2013-02-23 16:06:30 +0900

    mongoose を使って add を実装した

--- model/account.js
+++ model/account.js
@@ -2,10 +2,24 @@
  * model/account.js
  */
 
-function Account(db) {
-  this.db = db;
+var mongoose = require('mongoose');
+var ItemSchema = new mongoose.Schema({
+  date: Date,
+  amount: Number
+});
+mongoose.model('Item', ItemSchema);
+
+function Account(db_name) {
+  mongoose.connect('mongodb://localhost/' + db_name);
+
   this.add = function (item) {
-    this.db.items.push(item);
+    var Item = mongoose.model('Item');
+    var new_item = new Item();
+    new_item.date = item.date;
+    new_item.amount = item.amount;
+    new_item.save(function(err){
+      if(err){ console.log(err); }
+    });
   }
 }
 
